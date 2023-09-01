83a6e958560ee73f8eda5e265fdf5a5935c59782
mirzomumin <mirzomumin@list.ru>
2023-09-01 19:52:14 +0500

    add auth middleware

---
+++ middleware.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"context"
+	"github.com/dgrijalva/jwt-go"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type","application/json")
+		cookie, err := r.Cookie("SESSTOKEN")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"error": "Unauthorized!"}`))
+		} else {
+			jwtToken := cookie.Value
+			token, err := jwt.Parse(jwtToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte(SECRET_KEY), nil
+			})
+
+			if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+				ctx := context.WithValue(r.Context(), "props", claims)
+				// Access context values in handlers like this
+				// props, _ := r.Context().Value("props").(jwt.MapClaims)
+				next.ServeHTTP(w, r.WithContext(ctx))
+			} else {
+				fmt.Println(err)
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte(`{"error": "Unauthorized"}`))
+			}
+		}
+	})
+}
